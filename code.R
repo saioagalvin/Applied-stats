867b55d6fe12f9b7af226c0fa11750a1b88317ad
saioa <saioa.galvin@protonmail.com>
2026-03-10 12:03:40 +0000

    q1

---
+++ code.R
@@ -0,0 +1,7 @@
+# read in data
+elections <- read.table("BushGore.dat", header = TRUE)
+
+# hypothesis testing
+# H_0 : 2000 outcome (Buchanan) is consistent with 1996 (Perot)
+# H_1 : Voting irregularities are observed in 2000 compared to 1996
+
